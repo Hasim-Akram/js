17a7df8f983296735e7b7da8d98e8c5574ae8cb0
Hasim-Akram <101513084+Hasim-Akram@users.noreply.github.com>
2022-09-25 22:49:48 +0600

    2nn

---
+++ index.js
@@ -0,0 +1,40 @@
+var num1 = 30;
+var num2 = 30;
+var sum1 = num1 + num2;
+
+let name = "Hasim Akram";
+
+console.log(sum1);
+
+console.log(typeof name);
+
+
+
+let promise = "I promise I will do this course within a month";
+
+console.log(promise.toLowerCase());
+console.log(promise.toLocaleUpperCase());
+console.log(promise.indexOf('will'));
+console.log(promise.split(' '));
+
+var one = 0.1;
+var two = 0.2;
+
+
+//two = parseInt(two);
+
+
+console.log(one + two);
+
+
+var pos = -5;
+
+var absolutem = Math.abs(pos);
+
+
+
+console.log(absolutem);
+
+
+var date = new Date('1995-07-10');
+console.log(date);
