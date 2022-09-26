9b892601f4f31b37af3f45971793ebc059cb8ba2
Hasim-Akram <101513084+Hasim-Akram@users.noreply.github.com>
2022-09-26 21:00:20 +0600

    funtion loop

--- index.js
+++ index.js
@@ -38,3 +38,124 @@ console.log(absolutem);
 
 var date = new Date('1995-07-10');
 console.log(date);
+
+//array js
+
+var friendAge = [12, 17, 20, 23, 12, 58, 63, 2, 56, 24, 12, 1, 6];
+console.log(friendAge.length);
+
+console.log(friendAge[1]);
+friendAge[3] = 25;
+
+var hasimAge = friendAge[3];
+console.log(hasimAge);
+console.log(friendAge.indexOf(25));
+//push mean array updated its element
+friendAge.push(260, 102, 222);
+console.log(friendAge);
+console.log(friendAge.length);
+//pop mean delete last item
+friendAge.pop();
+console.log(friendAge);
+//add remove add beggining array
+friendAge.unshift(260, 102, 222);
+console.log(friendAge);
+friendAge.shift(260, 102, 222);
+console.log(friendAge);
+
+console.log(friendAge.slice(3, 10));
+
+
+//loop
+
+
+var numwh = 0;
+
+while (numwh <= 10) {
+    console.log(numwh);
+    numwh++;
+
+}
+
+var arrayll = [20, 30, 52, 33, 33, 32, 55];
+
+
+for (var i = 0; i <= arrayll.length; i++) {
+    console.log(arrayll[i]);
+}
+
+
+function Myfuncion() {
+    console.log('jan o baby');
+    console.log('this is called function');
+}
+Myfuncion();
+
+
+
+function double(paranum) {
+
+    var result = paranum * 2;
+    console.log(result)
+
+
+}
+double(10);
+double(200);
+double(555)
+
+
+
+//return
+
+
+function valureturn(retunval) {
+    var resultreturn = retunval * 2;
+    return resultreturn;
+}
+
+var firstval = valureturn(50);
+var secondval = valureturn(100);
+
+console.log(" return value ", firstval, secondval);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
